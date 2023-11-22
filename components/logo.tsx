ec9f73ae8b6f3a1b195a3dd470bbd28cad868e64
kemalkujovic <kemalkujovic1111@gmail.com>
2023-11-22 07:28:35 +0100

    add sidebar & change name

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,8 +6,8 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-        <p className="text-lg text-neutral-700 pb-1">Taskify</p>
+        <Image src="/logo.jpg" alt="Logo" height={30} width={30} />
+        <p className="text-lg text-neutral-700 pb-1">Trello</p>
       </div>
     </Link>
   );
