b4c11ba96900aeb9c7455f59f385065a53ce7b61
kemalkujovic <kemalkujovic1111@gmail.com>
2023-11-25 08:45:50 +0100

    connect db && prisma

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,32 @@
+import { db } from "@/lib/db";
+
 const OrganizationPage = () => {
-  return <div>Organization Page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationPage;
